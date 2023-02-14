89d3ab13bf6515de0283c7b139202ffb4fff85c9
seifb <seifb@LAPTOP-EJFJP1RL>
2023-02-14 20:28:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Signup from './Pages/signup/SignUp'
+import Footer from './components/footer/Footer'
+function App() {
+  return (
+    <>
+<Signup/>
+<Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/signup/SignUp.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import "./signup.css"
+import Imge from "./se.png"
+import Icon from "./gmail.png"
+const SignUp = () => {
+    const jarib=()=>{
+        alert("button activ!!!")
+    }
+  return (
+    <div>
+       <div><div class="sign-up-Zg3mej" data-id="3:44">
+<input type="email" div class="rectangle-950-6AIlOd" data-id="3:45" placeholder='Email'/>/
+
+<input type="password" div class="rectangle-952-6AIlOd" data-id="3:46" placeholder='Mot de passe'/>
+
+<input type="password" class="rectangle-953-6AIlOd" data-id="3:47"placeholder='Confirmer mot de passe'/>
+
+<input type="text" class="rectangle-951-6AIlOd" data-id="3:48" placeholder='Nom et Prénom'/>
+<p class="sinscrire-6AIlOd" data-id="3:49">S'inscrire</p>
+
+<div class="group-612-6AIlOd" data-id="3:50">
+<input type="checkbox"class="tick-square-AEmqbd decoded loaded"data-id="3:51"/>
+<div class="vuesax-bold-tick-square-uXDjwz" data-id="I3:51;6774:2349">
+</div>
+
+<p class="jai-lu-et-jaccepte-les-conditions-générales-H0LNIy" data-id="3:52">J'ai lu et j'accepte les conditions générales.</p></div>
+<p class="continue-avec-6AIlOd" data-id="3:53">Continue avec</p>
+<p class="ou-avec-votre-adresse-e-mail-6AIlOd" data-id="3:54">Ou avec votre adresse e-mail</p>
+
+<p class="avez-vous-déjà-un-compte-connexion-6AIlOd" data-id="3:59">Avez vous déjà un compte! Connexion</p>
+<div class="group-611-6AIlOd" data-id="3:60">
+<button onClick={jarib} class="rectangle-955-swKQv0" data-id="3:61">
+
+<p class="sinscrire-swKQv0" data-id="3:62">S'inscrire</p></button></div>
+<div class="group-615-6AIlOd" data-id="3:78">
+<button onClick={jarib}class="rectangle-953-qNyFR6" data-id="3:79">
+
+<div class="group-614-qNyFR6" data-id="3:80">
+<img src={Icon} class="google__g__logo-2-BMdyMi decoded" data-id="3:81"/>
+<p class="gmail-BMdyMi" data-id="3:87">Gmail</p></div>
+</button>
+</div>
+<div class="group-616-6AIlOd" data-id="3:88">
+<button onClick={jarib} class="rectangle-952-tMLdd3" data-id="3:89">
+
+<div class="group-613-tMLdd3" data-id="3:90">
+<div class="group-610-vNvGum" data-id="3:91">
+<div class="rectangle-957-jQdYQB" data-id="3:92">
+</div>
+<img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iOCIgaGVpZ2h0PSIxNSIgdmlld0JveD0iMCAwIDggMTUiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNy4wMzI5NyAyLjY5ODVDNi42MDc5NyAyLjYxMzUgNi4wMzM5OCAyLjU1IDUuNjcyOTggMi41NUM0LjY5NTQ4IDIuNTUgNC42MzE5OCAyLjk3NSA0LjYzMTk4IDMuNjU1VjQuODY1NUg3LjA3NTQ4TDYuODYyNDggNy4zNzNINC42MzE5OFYxNUgxLjU3MjQ5VjcuMzczSDBMMCA0Ljg2NTVIMS41NzI0OVYzLjMxNDVDMS41NzI0OSAxLjE5IDIuNTcwOTkgMCA1LjA3Nzk5IDBDNS45NDg5OCAwIDYuNTg2NDggMC4xMjc1MDEgNy40MTQ5OCAwLjI5NzUwMUw3LjAzMjk3IDIuNjk4NVoiIGZpbGw9IndoaXRlIi8+Cjwvc3ZnPgo=" class="vector-jQdYQB decoded loaded" data-id="3:93"/>
+</div>
+<p class="facebook-vNvGum" data-id="3:94">Facebook</p></div>
+</button>
+</div>
+<img src={Imge} class="image-1-6AIlOd decoded loaded" data-id="4:7"/>
+</div>
+<div class="overlay-window" id="overlay-main-js">
+</div>
+</div>
+    </div>
+  )
+}
+
+export default SignUp

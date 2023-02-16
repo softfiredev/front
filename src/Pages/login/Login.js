17967316549ff8b822d92199daa3a0e3b1fee066
seifba007 <98665958+seifba007@users.noreply.github.com>
2023-02-16 04:04:22 +0100

    add

--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -2,83 +2,87 @@ import React from 'react'
 import "./login.css"
 import { Link } from 'react-router-dom';
 import Imge from "../../assest/image1.png"
-import { TextField, Grid } from '@mui/material'
+import { TextField, Grid, Button } from '@mui/material'
 import Checkbox from '@mui/material/Checkbox';
 import { Facebook } from '@material-ui/icons';
-import Icongoogle from '../../assest/google.png';
+
 const Login = () => {
   const test = () => {
     alert("button active !!")
   }
 
   return (
-    <div className='main'>
+    <div className='login'>
 
-      <Grid container direction="row" spacing={1}>
-        <Grid item xs>
-          <div> <img className='im' src={Imge} alt="icon" /></div>
+      <Grid
+        container spacing={8}
+      >
+        <Grid item >
+          <img className='im' src={Imge} alt="icon" />
         </Grid>
-        <Grid item container direction="column" xs spacing={2}>
-        <br />  <br />
-        
-          <Grid item xs={5} md={1}>
-            <div className='Connexion'>Connexion</div>
+        <Grid item >
 
-          </Grid>
-          <Grid item xs={1} md={1} >
-            <div className='content' >Continue avec</div>
-            <br />
-          </Grid>
-          <Grid item xs={1} md={1}>
-            <Grid container direction="row" spacing={4}>
-              <button className='gmail' onClick={test}>
-                <div className="grid-container">
-                  <div className="grid-item"><img className="icongoogle" src={Icongoogle} alt="icon" /></div>
-                  <div className="grid-item">Gmail</div>
-                </div>
-              </button>
-              <button className='facebook' onClick={test}>
-                <div className="grid-container2">
-                  <div><Facebook className='icon' /></div>
-                  <span className="textfacebook">Facebook</span>
-
-                </div>
-              </button>
+          <Grid
+            container direction="column" spacing={2.7}
+          ><br />
+            <Grid item >
+              <div className='Connexion'>Connexion</div>
+            </Grid >
+            <Grid item >
+              <div className='content' >Continue avec</div>
             </Grid>
-          </Grid>
-          <Grid item xs={6} md={1}>
-            <div className='adr'>Ou avec votre adresse e-mail</div>
-          </Grid>
+            <Grid item >
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+              >
+                <Grid item >
+                  <Button ><div className='icong'><i class="fab fa-google fa-1x" ></i></div><div className="textfbk">Gmail</div></Button>
+                </Grid>
+                <Grid item>
+                  <Button ><div className='iconf'><Facebook style={{ width: "20px", height: "20px" }} /></div> <div className='textfbk'>Facebook</div></Button>
+                </Grid>
+
+              </Grid>
+            </Grid>
+            <Grid item>
+              <div className='adr'>Ou avec votre adresse e-mail</div>
+            </Grid>
+         
+            <Grid item>
+              <TextField className='bnt' label="Email" />
+            </Grid>
+            <Grid item>
+              <TextField className='bnt' label="Mot de passe" type="password" />
+            </Grid>
+            <Grid item>
+            <Grid
+  container
+  direction="row-reverse"
+  justifyContent="flex-start"
+  alignItems="baseline"
+>
+              <p className='obli'>Mot de passe oublié?</p>
+            </Grid>
+            </Grid>
+            
+            <Grid item   container spacing={2}>
+              <Checkbox style={{ color: " #1A110A", }} /><span ><div class="Souviens">Souviens-toi de moi</div></span>
+            </Grid>
+            <Grid item>
+            <button className='bnt1' onClick={test}>
+                <div className='discription2'>Connexion</div></button>
+            </Grid>
+            <Grid item>
+              <div>Avez vous déjà un compte!  <Link to="/Signup" className="textc"> S'inscrire</Link></div>
+            </Grid>
+
+          </Grid >
 
 
-          <Grid item >
-            <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Email" />
-          </Grid>
-          <br />
-          <Grid item xs={1}>
-            <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Mot de passe" type="password" />
-          </Grid>
-
-
-
-          <Grid item>
-            <div className='oblie'><Link to="#">Mot de passe oublié?</Link></div>
-          </Grid>
-
-          <Grid item >
-            <Checkbox style={{ color: " #1A110A", }} /><span >Souviens-toi de moi</span>
-          </Grid>
-
-          <Grid item >
-            <button className='bnt' onClick={test}>
-              <div className='discription2'>Connexion</div></button>
-          </Grid>
-
-          <Grid item > <br />
-            <div  >Vous n'avez pas de compte !<Link to="/Signup" className="textc"> S'inscrire</Link></div>
-          </Grid>
-        </Grid>
-
+        </Grid >
       </Grid>
     </div>
 
--- src/Pages/signup/SignUp.js
+++ src/Pages/signup/SignUp.js
@@ -1,91 +1,87 @@
 import React from 'react'
 import "./signup.css"
 import Imge from "../../assest/image1.png"
-
-import { TextField, Grid } from '@mui/material'
-
+import { TextField, Grid,Button } from '@mui/material'
 import Checkbox from '@mui/material/Checkbox';
 import { Facebook } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-import Icongoogle from '../../assest/google.png';
 const Signup = () => {
   const test = () => {
     alert("button active !!")
   }
   return (
     <div>
-      <div className='main'>
+      <div className='signup'>
 
-        <Grid container direction="row" spacing={2}>
-          <Grid item xs>
-            <div> <img className='im' src={Imge} alt="icon" /></div>
-          </Grid>
-          <Grid item container direction="column" xs spacing={2}>
+      <Grid
+        container spacing={8}
+      >
+        <Grid item >
+          <img className='im' src={Imge} alt="icon" />
+        </Grid>
+        <Grid item >
 
-            <Grid item xs={5} md={1}>
+          <Grid
+            container direction="column" spacing={2.97}
+          >
+            <Grid item ><br/><br/>
               <div className='Connexion'>S'inscrire</div>
-
-            </Grid>
-            <Grid item xs={1} md={1} >
+            </Grid >
+            <Grid item >
               <div className='content' >Continue avec</div>
-
             </Grid>
-            <Grid item xs={1} md={1}>
-              <Grid container direction="row" spacing={3}>
-                <button className='gmail' onClick={test}>
-                  <div className="grid-container">
-                    <div className="grid-item"><img className="icongoogle" src={Icongoogle} alt="icon" /></div>
-                    <div className="grid-item">Gmail</div>
-                  </div>
-                </button>
-                <button className='facebook' onClick={test}>
-                  <div className="grid-container2">
-                    <div><Facebook className='icon' /></div>
-                    <div className="textfacebook">Facebook</div>
+            <Grid item >
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+              >
+                <Grid item >
+                  <Button ><div className='icong'><i class="fab fa-google fa-1x" ></i></div><div className="textgml">Gmail</div></Button>
+                </Grid>
+                <Grid item>
+                  <Button ><div className='iconf'><Facebook style={{ width: "20px", height: "20px" }} /></div> <div className='textfbk'>Facebook</div></Button>
+                </Grid>
 
-                  </div>
-                </button>
               </Grid>
             </Grid>
-            <Grid item xs={6} md={1}>
+            <Grid item>
               <div className='adr'>Ou avec votre adresse e-mail</div>
             </Grid>
-            <Grid item >
-              <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Nom et Prénom" />
-            </Grid>
-            <Grid item >
-              <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Email" />
-            </Grid>
-            <Grid item >
-              <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Mot de passe" type="password" />
-            </Grid>
-            <Grid item xs={1}>
-              <TextField style={{ width: "416px", height: "52px" }} fullWidth label="Confirmer mot de passe" type="password" />
-            </Grid>
-
-
-
+         
             <Grid item>
-              <div className='oblie'><Link to="#">Mot de passe oublié?</Link></div>
+              <TextField className='bnt' label="Nom et Prénom" />
+            </Grid>
+            <Grid item>
+              <TextField className='bnt' label="Email" />
+            </Grid>
+            <Grid item>
+                <TextField className='bnt' label="Mot de passe" type="password" />
+              </Grid>
+              <Grid item >
+                <TextField className='bnt' label="Confirmer mot de passe" type="password" />
+              </Grid>
+        
+          
+            <Grid item>
+              <Checkbox style={{ color: " #1A110A", }} /><span>j'ai lu et j'accepte <span className='text2'>les conditions générales.</span></span>
+            </Grid>
+            <Grid item>
+            <button className='bnt1' onClick={test}>
+                <div className='discription2'>S'inscrire</div></button>
+            </Grid>
+            <Grid item>
+              <div>Avez vous déjà un compte!  <Link to="/login" className="textc"> Connexion</Link></div>
             </Grid>
 
-            <Grid item >
-              <Checkbox style={{ color: " #1A110A", }} /><span className='text3' >J'ai lu et j'accepte les conditions générales.</span>
-            </Grid>
+          </Grid >
 
-            <Grid item >
-              <button className='bnt' onClick={test}>
-                <div className='discription2'> S'inscrire</div></button>
 
-            </Grid>
-
-            <Grid item >
-              <div>Avez vous déjà un compte!  <Link to="/Login" className="textc">Connexion</Link></div>
-            </Grid>
-          </Grid>
-
-        </Grid>
+        </Grid >
+      </Grid>
       </div>
+
 
     </div>
   )

9841a264dcf22c90338c8a6cd8c2bdac33cf2aa7
seifba007 <98665958+seifba007@users.noreply.github.com>
2023-04-27 11:24:25 +0100

    new

--- src/components/composot-Profile/page-profile/Page.js
+++ src/components/composot-Profile/page-profile/Page.js
@@ -6,39 +6,12 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Avtr from "../../../assets/avtclient.png";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
-import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
-import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import { Visibility, VisibilityOff } from "@material-ui/icons";
 const Page = () => {
-  const Accordion = styled((props) => (
-    <MuiAccordion disableGutters elevation={0} square {...props} />
-  ))(({ theme }) => ({
-    border: theme.palette.divider,
-    "&:not(:last-child)": {
-      borderBottom: 0,
-      width: 618,
-    },
-    "&:before": {
-      display: "none",
-    },
-  }));
-  {
-  }
-  const AccordionSummary = styled((props) => (
-    <MuiAccordionSummary {...props} />
-  ))(({ theme }) => ({}));
 
-  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-    padding: theme.spacing(3),
-  }));
-  const [expanded, setExpanded] = React.useState("panel1");
 const[openchangePassword,setopenchangePassword]=useState(false)
-  const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
+const [opencoll, setopencoll] = React.useState(false);
+
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleOpenChanegPassword=()=>{
@@ -47,7 +20,15 @@ const[openchangePassword,setopenchangePassword]=useState(false)
   const handleCloseChanegPassword=()=>{
     setopenchangePassword(false)
   }
-  console.log("test",openchangePassword)
+ const changestyle=()=>{
+  setopencoll(true)
+
+ }
+ const changestyle2=()=>{
+  setopencoll(false)
+ }
+ 
+ const addr=[{nom:"119 Rue Wanes Chbil",adr:"71 Rue Galboun Ibn Al Hassen",Gouvernorat:"bembla",Ville:"bembla",Codepostal:"5200"},{nom:"119 Rue Wanes Chbil",adr:"71 Rue Galboun Ibn Al Hassen",Gouvernorat:"bembla",Ville:"bembla",Codepostal:"5200"},{nom:"119 Rue Wanes Chbil",adr:"71 Rue Galboun Ibn Al Hassen",Gouvernorat:"bembla",Ville:"bembla",Codepostal:"5200"}]
   return (
     <div>
       <div className="carts2">
@@ -157,27 +138,60 @@ const[openchangePassword,setopenchangePassword]=useState(false)
       </div>
       <br />
 
-      <Accordion
-        expanded={expanded === "panel"}
-        onChange={handleChange("panel")}
-      >
-        <AccordionSummary
-          aria-controls="panel1d-content"
-          id="panel1d-header"
-          style={{ width: "650px" }}
-        >
-          <Typography style={{ width: "650px" }} className="typography">
+   
+    
+            <div  onClick={changestyle}>
             <div className="box3-page">
               <h2>Mes adresses</h2>
-              <div className="rowbnt-page">
+              <div className={opencoll ?"box-hed":"rowbnt-page"}>
                 <AddCircle size="22" color="#E9B949" variant="Bold" />
                 <div>Ajouter une nouvelle adresse</div>
               </div>
+
+
+              <div className={opencoll && addr.length!=0 ?"col-page012":"box-hed"}>
+            <div className="txt201-page">. Maison</div>
+              {addr.map((obj) => (
+          <>
+            <div className="row-page032">
+            <div>{obj.nom},</div>
+          <div>{obj.Ville},</div>
+          <div>{obj.Gouvernorat},</div>
+          <div>{obj.Codepostal}</div>
             </div>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className="typographyReponse-page">
+           
+          </>
+        ))}
+       <div className="row-page"> <div className="txt200-page">Modifier</div> <div className="txt200-page">Supprimer</div></div>
+         </div>   
+              
+         <div className={opencoll && addr.length!=0 ?"col-page012":"box-hed"}>
+            <div className="txt201-page">. Lieu de travail</div>
+              {addr.map((obj) => (
+          <>
+            <div className="row-page032">
+            <div>{obj.nom},</div>
+          <div>{obj.Ville},</div>
+          <div>{obj.Gouvernorat},</div>
+          <div>{obj.Codepostal}</div>
+            </div>
+           
+          </>
+        ))}
+       <div className="row-page"> <div className="txt200-page">Modifier</div> <div className="txt200-page">Supprimer</div></div>
+         </div>
+         
+         <br/>
+              
+
+
+
+
+                </div>
+            </div>
+          
+  <div className={opencoll ?"box-blok":"box-hed"}>
+  <div className="typographyReponse-page">
             <hr className="line-page" />
             <br />
             <div className="col-page">
@@ -223,7 +237,7 @@ const[openchangePassword,setopenchangePassword]=useState(false)
               </div>
 
               <div className="minirow-page">
-                <button className="bnt3-page">
+                <button className="bnt3-page" onClick={changestyle2}>
                   <p className="txtbnt3-page">Annuler</p>
                 </button>
                 <button className="bnt4-page">
@@ -232,8 +246,10 @@ const[openchangePassword,setopenchangePassword]=useState(false)
               </div>
             </div>
           </div>
-        </AccordionDetails>
-      </Accordion>
+
+  </div>
+    
+    
     </div>
   );
 };

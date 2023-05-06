79a96012e4571a4c74b78ca1b5339db5c4eae61e
Bouattay Mahmoud <mahmoudbouattay178@gmail.com>
2023-05-06 20:29:51 +0200

    commit  mahmoud

--- src/components/composot-Profile/page-profile/Page.js
+++ src/components/composot-Profile/page-profile/Page.js
@@ -106,7 +106,7 @@ const [avatar,setavatar]=useState()
 const [refreshpage,setrefreshpage]=useState()
 useEffect(() => {
   dispatch(getIdentiteClientt(props.user.id));
-},[]);
+},[refreshpage]);
 const addresses=clientData?.client?.adresses
 const changeIdentite=()=>{
    const data= new FormData() ; 
